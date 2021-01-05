724b4434165960a83d3f33fe5407ff2179006268
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-01-05 01:31:24 +0100

    cleanup echo debugging

--- btc-node-entrypoint.sh
+++ btc-node-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
-    echo "exec command $1"
     exec "$@"
 else
     apt-get update && apt-get install -y jq net-tools
-    echo "exec bitcoind"
     exec bitcoind
 fi
