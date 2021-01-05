632fd3391b6f768c0ffac00123a8a8c92371c39a
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-06 00:10:04 +0100

    utility to cleanup blockhains

---
+++ cleanup-blockchain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo rm -Rf hansel-btc/regtest
+sudo rm -Rf gretel-btc/regtest
+sudo rm -Rf bitcoinjs-regtest-server/bitcoin-data/regtest
+sudo rm -Rf explorer/bitcoin
+sudo rm -Rf explorer/logs
+sudo rm -Rf explorer/electrs_bitcoin_db
