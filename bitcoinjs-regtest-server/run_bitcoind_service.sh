f684ab3d0e8332aca3965938d03b0ca21bf346b3
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-05 21:27:42 +0100

    disable genereate to address frm bitcoinjs regtest server bootstrap

--- bitcoinjs-regtest-server/run_bitcoind_service.sh
+++ bitcoinjs-regtest-server/run_bitcoind_service.sh
@@ -2,5 +2,5 @@
 
 /usr/bin/bitcoind -server -regtest -txindex -zmqpubhashtx=tcp://127.0.0.1:30001 -zmqpubhashblock=tcp://127.0.0.1:30001 -rpcworkqueue=32 &
 disown
-sleep 2
-/usr/bin/bitcoin-cli -regtest generate 432
+#sleep 2
+#/usr/bin/bitcoin-cli -regtest generate 432
