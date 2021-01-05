7255c24811e30e6438b510db921569d17d79e491
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-01-05 01:19:19 +0100

    apt only on entrypoint first run

--- btc-node-entrypoint.sh
+++ btc-node-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y jq
-
 if [ -n "$1" ]; then
     echo "exec command $1"
     exec "$@"
 else
+    apt-get update && apt-get install -y jq net-tools
     echo "exec bitcoind"
     exec bitcoind
 fi
