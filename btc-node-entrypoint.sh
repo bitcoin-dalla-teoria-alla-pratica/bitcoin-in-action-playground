0b0ac6e517c11661b638f10ac87ef36561ca466c
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-06 18:25:10 +0100

    chmod +x bitcoin-cli and bitcoind on container startup

--- btc-node-entrypoint.sh
+++ btc-node-entrypoint.sh
@@ -4,5 +4,7 @@ if [ -n "$1" ]; then
     exec "$@"
 else
     apt-get update && apt-get install -y jq net-tools
+    chmod +x /usr/bin/bitcoin-cli
+    chmod +x /usr/bin/bitcoind
     exec bitcoind
 fi
