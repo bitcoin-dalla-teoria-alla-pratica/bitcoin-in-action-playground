d2fb5e156cc017e6929166091fe46bd33f77e8f4
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-06 23:57:44 +0100

    all nodes script able to exec command with params

--- all-nodes-cmd.sh
+++ all-nodes-cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "==== hansel-btc"
-docker exec -ti playground_hansel-btc_1 $1
+docker exec -ti playground_hansel-btc_1 $@
 
 echo "==== gretel-btc"
-docker exec -ti playground_gretel-btc_1 $1
+docker exec -ti playground_gretel-btc_1 $@
