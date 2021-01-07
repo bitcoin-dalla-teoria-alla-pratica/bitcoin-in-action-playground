b9e7f2a1b7d0c144f27802dd613ef52945193642
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-01-07 02:23:14 +0100

    update deps install entrypoint

--- btc-node-entrypoint.sh
+++ btc-node-entrypoint.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+
 if [ -n "$1" ]; then
     exec "$@"
 else
     apt-get update
     apt-get install -y \
         autoconf \
-        base58 \
+        build-essential \
         bc \
         git \
         jq \
@@ -16,21 +17,35 @@ else
         libtool \
         net-tools \
         openssl \
+        python-pip \
+        pkg-config \
         sed \
-        xxd
+        xxd \
+        wget
+    pip install base58
     # btcdeb setup
-    cd /opt
-    git clone https://github.com/bitcoin-core/btcdeb.git
-    cd btcdeb
-    chmod +x ./autogen.sh
-    ./autogen.sh
-    chmod +x  ./configure
-    ./configure
-    make
-    make install
+    if ! command -v btcdeb &> /dev/null
+    then
+	    cd /opt
+	    wget https://github.com/bitcoin-core/btcdeb/archive/0.3.20.tar.gz
+	    tar -xzf 0.3.20.tar.gz
+	    cd btcdeb-0.3.20
+	    chmod +x ./autogen.sh
+	    ./autogen.sh
+	    chmod +x  ./configure
+	    ./configure
+	    make
+	    make install
+    fi
     # bitcoin core setup
     chmod +x /usr/bin/bitcoin-cli
     chmod +x /usr/bin/bitcoind
+
+    # bizantino utility
+    if ! command -v hello.sh &> /dev/null
+    then
+	echo 'export PATH=$PATH:/opt/shared/utility' >> /root/.bashrc
+    fi
 
     # foreground bitcoin daemon
     exec bitcoind
