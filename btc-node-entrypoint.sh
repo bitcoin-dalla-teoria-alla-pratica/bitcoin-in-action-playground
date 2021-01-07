7468ad6b9178206eea3cd354c28181c1c98263ba
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-07 01:24:56 +0100

    bit del mestiere

--- btc-node-entrypoint.sh
+++ btc-node-entrypoint.sh
@@ -3,8 +3,35 @@
 if [ -n "$1" ]; then
     exec "$@"
 else
-    apt-get update && apt-get install -y jq net-tools
+    apt-get update
+    apt-get install -y \
+        autoconf \
+        awk \
+        base58 \
+        bc \
+        cut \
+        git \
+        jq \
+        libssl-dev \
+        libtool \
+        net-tools \
+        openssl \
+        sed \
+        xxd
+    # btcdeb setup
+    cd /opt
+    git clone git@github.com:bitcoin-core/btcdeb.git
+    cd btcdeb
+    chmod +x ./autogen.sh
+    ./autogen.sh
+    chmod +x  ./configure
+    ./configure
+    make
+    make install
+    # bitcoin core setup
     chmod +x /usr/bin/bitcoin-cli
     chmod +x /usr/bin/bitcoind
+
+    # foreground bitcoin daemon
     exec bitcoind
 fi
