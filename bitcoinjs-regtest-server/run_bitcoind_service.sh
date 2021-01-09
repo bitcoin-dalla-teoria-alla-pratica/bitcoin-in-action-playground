08df3d81db302e3adc9bef78e010abbf869cb0ee
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-09 18:32:59 +0100

    more entrypoint cleanup

--- bitcoinjs-regtest-server/run_bitcoind_service.sh
+++ bitcoinjs-regtest-server/run_bitcoind_service.sh
@@ -7,7 +7,4 @@ function on_sig_term() {
 
 trap on_sig_term SIGTERM
 
-/usr/bin/bitcoind -server -regtest -txindex -zmqpubhashtx=tcp://127.0.0.1:30001 -zmqpubhashblock=tcp://127.0.0.1:30001 -rpcworkqueue=32 &
-disown
-#sleep 2
-#/usr/bin/bitcoin-cli -regtest generate 432
+/usr/bin/bitcoind -zmqpubhashtx=tcp://127.0.0.1:30001 -zmqpubhashblock=tcp://127.0.0.1:30001 -rpcworkqueue=32
