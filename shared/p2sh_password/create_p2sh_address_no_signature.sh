f5c612fe15b125ccec9feef09807f5b96593d4b8
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-01-09 02:14:54 +0100

    clean output

--- shared/p2sh_password/create_p2sh_address_no_signature.sh
+++ shared/p2sh_password/create_p2sh_address_no_signature.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf  "\n\e[42m ######### P2SH #########\e[49m\n\n"
+#printf  "\n\e[42m ######### P2SH #########\e[49m\n\n"
 
 PASSWORD=$1
 # barno = 2c0a7a036ee138fe1e207676c436f6048703076cc6b8525a0ee3b84638976f0f
@@ -15,11 +15,11 @@ printf $SCRIPT > redeem_script.txt
 printf "\e[46m ---------- Redeem Script --------- \e[49m\n"
 cat redeem_script.txt
 
-printf "\n \n\e[46m ---------- scriptPubKey --------- \e[49m\n"
+#printf "\n \n\e[46m ---------- scriptPubKey --------- \e[49m\n"
 ADDR_SHA=`printf $SCRIPT | xxd -r -p | openssl sha256| sed 's/^.* //'`
 ADDR_RIPEMD160=`printf $ADDR_SHA |xxd -r -p | openssl ripemd160 | sed 's/^.* //'`
 printf $ADDR_RIPEMD160 > scriptPubKey.txt
-cat scriptPubKey.txt
+#cat scriptPubKey.txt
 
 #ADDRESS
 VERSION_PREFIX_ADDRESS=C4
--- shared/p2sh_password/main.sh
+++ shared/p2sh_password/main.sh
@@ -8,19 +8,22 @@ ADDR_DEST=`cat address_P2SH.txt`
 
 bitcoin-cli importaddress $ADDR_DEST
 #Mint 101 blocks and get reward to spend
-printf  "\n \e[31m######### spend from P2SH #########\e[39m \n"
+#printf  "\n \e[31m######### spend from P2SH #########\e[39m \n"
 bitcoin-cli generatetoaddress 101 $ADDR_DEST >> /dev/null
 TXID=$(bitcoin-cli listunspent 1 101 '["'$ADDR_DEST'"]' | jq -r '.[0].txid')
 VOUT=$(bitcoin-cli listunspent 1 101 '["'$ADDR_DEST'"]' | jq -r '.[0].vout')
 AMOUNT=$(bitcoin-cli listunspent 1 101 '["'$ADDR_DEST'"]' | jq -r '.[0].amount-0.001')
 
+printf  "\n \e[31m######### TX con UTXO P2SH #########\e[39m\n\n"
+echo "http://localhost:8094/regtest/tx/$TXID"
+
 TX_DATA=$(bitcoin-cli createrawtransaction '[{"txid":"'$TXID'","vout":'$VOUT'}]' '[{"'$ADDR_MITT'":'$AMOUNT'}]')
-bitcoin-cli decoderawtransaction $TX_DATA | jq
+#bitcoin-cli decoderawtransaction $TX_DATA | jq
 
 
-printf  "\n \e[31m######### Transaction data without scriptSig #########\e[39m \n"
-echo $TX_DATA
-printf  "\n \e[31m######### Add ScriptSig #########\e[39m \n"
+#printf  "\n \e[31m######### Transaction data without scriptSig #########\e[39m \n"
+#echo $TX_DATA
+#printf  "\n \e[31m######### Add ScriptSig #########\e[39m \n"
 
 TX_1=`printf $TX_DATA | cut -c 1-82`
 
@@ -32,22 +35,24 @@ SCRIPTSIG_LENGTH=$(char2hex.sh $(printf $SCRIPTSIG | wc -c))
 
 TX_SCRIPTSIG=$SCRIPTSIG_LENGTH$SCRIPTSIG
 
-printf  "\n \e[31m######### Transaction scriptSig #########\e[39m \n"
-echo $TX_SCRIPTSIG
+#printf  "\n \e[31m######### Transaction scriptSig #########\e[39m \n"
+#echo $TX_SCRIPTSIG
 
 TX_2=$(printf $TX_DATA | cut -c 85-)
 
 TX_RAW=$TX_1$TX_SCRIPTSIG$TX_2
 
-printf  "\n \e[31m######### Transaction RAW #########\e[39m \n"
-echo $TX_RAW
+#printf  "\n \e[31m######### Transaction RAW #########\e[39m \n"
+#echo $TX_RAW
 
-printf  "\n \e[31m######### Send transaction #########\e[0m\n\n"
+#printf  "\n \e[31m######### Send transaction #########\e[0m\n\n"
 TX_DATA=$(bitcoin-cli sendrawtransaction $TX_RAW)
+printf  "\n \e[31m######### USA PASSWORD PER RISCATTARE SALDO P2SH #########\e[0m\n\n"
+echo "http://localhost:8094/regtest/tx/$TX_DATA"
 
 if [[ -n $2 ]] ; then
   btcdeb --tx=$(printf $TX_RAW) --txin=$(bitcoin-cli getrawtransaction $TXID)
 fi
 
-printf  "\n \e[31m######### Mint 1 blocks #########\e[0m\n\n"
-bitcoin-cli generatetoaddress 1 $ADDR_MITT
+#printf  "\n \e[31m######### Mint 1 blocks #########\e[0m\n\n"
+bitcoin-cli generatetoaddress 1 $ADDR_MITT >> /dev/null
