28de9440d4434447081ddcc5131f032a3276a56f
alessandro_aglietti_brumbrum <alessandro.aglietti@brumbrum.it>
2021-01-09 19:07:41 +0100

    better trap for oz

--- oz/entrypoint.sh
+++ oz/entrypoint.sh
@@ -2,6 +2,8 @@
 
 function on_sig_term() {
     echo "====> $(date) RICEVUTO SIGTERM <===="
+    TAIL_PID=`ps -ef | grep "oz.log" | grep -v grep | awk '{print $2}'`
+    kill -15 $TAIL_PID
     exit 0
 }
 
@@ -15,4 +17,5 @@ else
     echo "$(date) - Oz container up and running!!" >> /var/log/oz.log
     echo "$(date) - Append to /var/log/oz.log to see message here!" >> /var/log/oz.log
     tail -f /var/log/oz.log
+    exit 0
 fi
