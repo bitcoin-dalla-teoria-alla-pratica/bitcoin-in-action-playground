ee2667d8bcbcb96b9682b71fba1e1aac99c9344b
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-01-12 02:24:26 +0100

    complete setup of blockchain reader

--- blockchain-reader/entrypoint.sh
+++ blockchain-reader/entrypoint.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+set -e
+
 EXPLORER_WAR=/usr/local/tomcat/webapps/explorer.war
 if [ -f "$EXPLORER_WAR" ]; then
     echo "$EXPLORER_WAR exists, skip download"
 else
     apt-get update
     apt-get install -y \
-        zip
+        zip \
+        maven
     
     rm -Rf /usr/local/tomcat/webapps
     # enable default tomcat apps
     cp -R /usr/local/tomcat/webapps.dist /usr/local/tomcat/webapps
-    cp /opt/manager/context.xml /usr/local/tomcat/webapps/manager/META-INF/context.xml
+    cp /opt/manager-webapp-context.xml /usr/local/tomcat/webapps/manager/META-INF/context.xml
     
     echo "$EXPLORER_WAR does not exist, download it!!"
 
-    wget https://github.com/yogh-io/bitcoin-transaction-explorer/releases/download/beta/explorer.war
-    mkdir -p WEB-INF/classes/
-    cp /opt/yoghurt.conf WEB-INF/classes/yoghurt.conf
-    zip -ur ./explorer.war WEB-INF
-    rm -Rf WEB-INF
-    mv ./explorer.war $EXPLORER_WAR
+    git clone https://github.com/yogh-io/bitcoin-yogh-explorer.git
+    cd bitcoin-yogh-explorer
+
+    # use our blockstream esplora node API
+    sed -i 's/https:\/\/blockstream.info/http:\/\/localhost:8094\/regtest/g' ./yogh-explorer-client/src/main/java/nl/yogh/wui/explorer/service/ElectrServiceAsyncImpl.java 
+    mvn install
+    
+    cp ./yogh-explorer-server/target/*.war ../webapps/explorer.war
 fi
 
 catalina.sh run
