c4d180a50824f87d973fdd0c44d0854296095a8d
Barno <barno.ab7@gmail.com>
2021-01-15 16:40:00 +0100

    aggiunto opzioni vim

--- wald/entrypoint.sh
+++ wald/entrypoint.sh
@@ -58,5 +58,13 @@ then
     ln -s /opt/wald/utility /usr/local/sbin
 fi
 
+#aggiunge le righe e la sintassi a vim
+cat << EOF > ~/.vimrc 
+set nu
+syntax on
+colorscheme pablo
+EOF
+
+
 # foreground bitcoin daemon
 bitcoind
