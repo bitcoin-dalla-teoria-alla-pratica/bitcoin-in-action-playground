a8de0e7fbcd93550b4338186a2c99f6ef673f2b7
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-01-16 19:42:30 +0100

    migliora leggibilita'

--- wald/p2sh_password/create_raw_tx.sh
+++ wald/p2sh_password/create_raw_tx.sh
@@ -5,18 +5,27 @@ REDEEM_SCRIPT_INPUT_DATA=$2 # SHA256(fegatini)
 REDEEM_SCRIPT=$3
 BENEFICIARIO=$4
 
-############# COSTRUZIONE TX CHE RISCATTA UTXO P2SH
+#### Recuperiamo tutti i dati necessari
 
-# Recuperiamo tutti i dettagli
-TX_WITH_MATURITY=`bitcoin-cli getrawtransaction $TXID_WITH_MATURITY 2`
-# Recuperiamo la UTXO
-P2SH_UTXO=`echo $TX_WITH_MATURITY | jq '.vout[0]'`
-# Recuperiamo l'indice della UTXO
+# Il JSON della UTXO della transazione
+P2SH_UTXO=`bitcoin-cli getrawtransaction $TXID_WITH_MATURITY 2 | jq '.vout[0]'`
+# dal quale recuperiamo...
+# ...l'indice della UTXO
 P2SH_UTXO_INDEX=`echo $P2SH_UTXO | jq -r '.n'`
-# Recuperiamo l'ammontare di bitcoin disponibili per la UTXO
+# ...l'ammontare di bitcoin disponibili per la UTXO
 P2SH_UTXO_AVAILABLE_AMOUNT=`echo $P2SH_UTXO | jq -r '.value'`
-# Sottraiamo la fee destinata al miner
+
+# Calcoliamo l'ammontare di bitcoin per il beneficiario
+# sottraendo la fee destinata al miner
 AMOUNT=`printf $P2SH_UTXO_AVAILABLE_AMOUNT | jq '. -0.001'`
+
+# Costruisco lo ScriptSig
+# In questo passaggio il REDEEM_SCRIPT viene "serializzato"
+# diventando una semplice operazione di push bytes sullo stack
+# da utilizzare durante la verifica dello ScriptPubKey
+SCRIPT_SIG=`btcc $REDEEM_SCRIPT_INPUT_DATA $REDEEM_SCRIPT`
+
+#### Costruzione della TX
 
 # Creiamo una transazione template che utilizza la UTXO P2SH e riscatta il saldo al destinatario
 TX_TEMPLATE=$(bitcoin-cli createrawtransaction '[{"txid":"'$TXID_WITH_MATURITY'","vout":'$P2SH_UTXO_INDEX'}]' '[{"'$BENEFICIARIO'":'$AMOUNT'}]')
@@ -25,17 +34,18 @@ TX_TEMPLATE=$(bitcoin-cli createrawtransaction '[{"txid":"'$TXID_WITH_MATURITY'"
 # che contiene la UTXO P2SH
 TX_1=`printf $TX_TEMPLATE | cut -c 1-82`
 
-# Costruisco lo ScriptSig
-SCRIPT_SIG=`btcc $REDEEM_SCRIPT_INPUT_DATA $REDEEM_SCRIPT`
-SCRIPT_SIG_SERIALIZED=`btcc $SCRIPT_SIG`
-
 # Recuperiamo l'intestazione esadecimale della transazione di output
 # che contiene BENEFICIARIO
 TX_2=$(printf $TX_TEMPLATE | cut -c 85-)
 
+# Quando si va ad aggiungere lo ScriptSig
+# ad una transazione bisogna prependerlo con
+# un valore esadecimale che ne indica la quantita' di byte
+SCRIPT_SIG_WITH_LENGTH=`btcc $SCRIPT_SIG`
+
 # Costruiamo l'esadecimale completo della transazione che
 # riscatta i fondi del P2SH dimostrando di conoscere lo script
 # e li invia ad BENEFICIARIO
-TX_RAW=$TX_1$SCRIPT_SIG_SERIALIZED$TX_2
+TX_RAW=$TX_1$SCRIPT_SIG_WITH_LENGTH$TX_2
 
 echo $TX_RAW
